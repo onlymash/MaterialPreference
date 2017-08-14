fa95719339ed533e085cdefb1b5ce3670cf1f502
Rikka <rikka@xing.moe>
2017-08-15 01:33:56 +0800

    add SwitchPreferenceCompat for pre-21

---
+++ preference-switchcompat/src/main/java/moe/shizuku/preference/SwitchPreferenceCompat.java
@@ -0,0 +1,26 @@
+package moe.shizuku.preference;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import moe.shizuku.preference.switchcompat.R;
+
+
+public class SwitchPreferenceCompat extends SwitchPreference {
+
+    public SwitchPreferenceCompat(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    public SwitchPreferenceCompat(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr, 0);
+    }
+
+    public SwitchPreferenceCompat(Context context, AttributeSet attrs) {
+        this(context, attrs, R.attr.switchPreferenceCompatStyle);
+    }
+
+    public SwitchPreferenceCompat(Context context) {
+        this(context, null);
+    }
+}
